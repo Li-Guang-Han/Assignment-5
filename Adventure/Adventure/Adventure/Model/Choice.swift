052fc3b081792a587f76a9cfaf792851772904a5
Garrett Li <166572446+Li-Guang-Han@users.noreply.github.com>
2025-07-05 11:59:14 -0500

    Update Choice.swift

--- Adventure/Adventure/Adventure/Model/Choice.swift
+++ Adventure/Adventure/Adventure/Model/Choice.swift
@@ -1,15 +1,1 @@
-//
-//  Choice.swift
-//  Adventure
-//
-//  Created by Garrett Li on 6/25/25.
-//
 
-
-import Foundation
-
-struct AdventureChoice {
-    let title: String
-    let nextChoices: [AdventureChoice]?
-    let isSuccessfulEnd: Bool?
-}
